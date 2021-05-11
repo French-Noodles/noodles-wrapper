3472463bd58ab224e0167240a2931602b3cb1e37
French Noodles <jhonbardakji84@gmail.com>
2021-05-12 01:19:03 +0200

    first working version

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-# noodles-wrapper
-
-Please dont download this yet it is not complete
-=======
-# Noodles-wrapper
->>>>>>> 0831f193decfb3795c22357f624f5c4c420c7825
+#noodles-wrapper
+Please dont use, its not done yet
--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const baseurl = 'https://frenchnoodles.xyz/api/endpoints/'
 
 async function request(endpoint, input='') {
     const res = await fetch(`${baseurl}${endpoint}/?${input}`, {
-        headers: {'User-Agent': `noodles-wrapper@${process.env.npm_package_version} by French Noodles#4000`}
+        headers: {'User-Agent': `noodles-wrapper@${process.env.npm_package_version}(https://www.npmjs.com/package/noodles-wrapper) by French Noodles#4000`}
     });
     return res
 }
 
 
-module.exports.lisastage = function (text) {
+async function lisastage(text) {
     if(!text) return console.error('A field was left empty in the noodles request function')
-    const input = encodeURIComponent(text)
+    const input = `text=${encodeURIComponent(text)}`
     const response = await request('lisastage', input)
     return response.buffer()
 }
+
+module.exports.lisastage = lisastage
